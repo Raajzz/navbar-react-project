186a6f3719128923c0e0eb751ba3ac52a465da76
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-01-06 00:04:00 +0530

    need to work on below 767 vw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from "react";
-import logo from './img/logo.svg'
+import React, { useEffect, useState } from "react";
+import logo from "./img/logo.svg";
 import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import { FiFacebook, FiTwitter, FiGithub } from "react-icons/fi";
-import { AiFillLinkedin } from "react-icons/ai"
+import { AiFillLinkedin } from "react-icons/ai";
 
 // , AiFillLinkedin, FiGithub
 
-const App = () => {
+const MaxComponent = () => {
   return (
-    <div className=" bg-slate-400 h-24 grid grid-cols-3">
-      {/* LOGO */}
-      <div className="vert-center">
-        <img
-          src={logo}
-          alt=""
-          className=" inline-block mx-5 lg:ml-16 2xl:ml-20 w-48 "
-        />
-      </div>
+    <>
       <div className="vert-center mx-auto">
         <Router>
           <Link to="#" className=" md:nav-item">
@@ -39,7 +31,7 @@ const App = () => {
       </div>
       {/* SOCIAL ICONS */}
       {/* // import { FiFacebook, FiTwitter, AiFillLinkedin, FiGithub } from "react-icons"; */}
-      <div className="vert-center mx-auto absolute md:right-5 lg:right-32 xl:right-52 right-2 h-24">
+      <div className="vert-center mx-auto absolute  md:right-7 lg:right-32 xl:right-52 h-24">
         <Router>
           <Link to="#" className=" md:nav-item">
             <FiFacebook />
@@ -55,6 +47,44 @@ const App = () => {
           </Link>
         </Router>
       </div>
+    </>
+  );
+};
+
+const MinComponent = () => {
+  return <>Hello</>;
+};
+
+const App = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const SMALLSIZE = 767;
+
+  const resizeHandler = () => {
+    setInnerWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`bg-slate-400 h-24 grid ${
+        innerWidth <= SMALLSIZE ? "grid-cols-2" : "grid-cols-3"
+      }`}
+    >
+      {/* LOGO */}
+      <div className="vert-center">
+        <img
+          src={logo}
+          alt=""
+          className=" inline-block mx-5 lg:ml-16 2xl:ml-20 w-48 "
+        />
+      </div>
+      {innerWidth <= SMALLSIZE ? <MinComponent /> : <MaxComponent />}
     </div>
   );
 };
